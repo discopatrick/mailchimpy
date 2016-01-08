832d222b3e213d35b77ddc328bbade4ca7f3903c
discopatrick <git@discopatrick.me>
2016-01-08 18:30:49 +0000

    adds gulp task for linting, removes gruntfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
 
-gulp.task('default', function() {
-	
-});
+gulp.task('default', shell.task([
+  'virtualenv/bin/autopep8 ./mailchimpy --recursive --in-place',
+  'virtualenv/bin/autopep8 ./tests --recursive --in-place'
+]))

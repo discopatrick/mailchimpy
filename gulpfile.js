2f8f1700e1af92afba6b6180b1c87013edd2780b
discopatrick <git@discopatrick.me>
2016-01-08 19:50:37 +0000

    creates default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 
-gulp.task('default', shell.task([
+gulp.task('lint-src', shell.task([
   'virtualenv/bin/autopep8 ./mailchimpy --recursive --in-place',
+]));
+
+gulp.task('lint-tests', shell.task([
   'virtualenv/bin/autopep8 ./tests --recursive --in-place'
-]))
+]));
+
+gulp.task('default', ['lint-src', 'lint-tests']);

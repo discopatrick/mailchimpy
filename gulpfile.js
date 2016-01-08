1be1340167867d73805bd5966611c5144fda177f
discopatrick <git@discopatrick.me>
2016-01-08 20:26:47 +0000

    adds gulp task for testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 
-gulp.task('lint-src', shell.task([
+gulp.task('lint:src', shell.task([
   'virtualenv/bin/autopep8 ./mailchimpy --recursive --in-place',
 ]));
 
-gulp.task('lint-tests', shell.task([
+gulp.task('lint:tests', shell.task([
   'virtualenv/bin/autopep8 ./tests --recursive --in-place'
 ]));
 
-gulp.task('default', ['lint-src', 'lint-tests']);
+gulp.task('test', shell.task([
+  'virtualenv/bin/python -m unittest'
+]));
+
+gulp.task('watch', function() {
+  gulp.watch('mailchimpy/**/*.py', ['test']);
+  gulp.watch('tests/**/*.py', ['test']);
+});
+
+gulp.task('default', ['watch']);
